6910cf960329b98a73453b52eb7bdbdab70592d8
Eshan Gupta <eshan1925@gmail.com>
2021-03-21 03:56:54 +0530

    Finished the Project.

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Home(),
-    );
+    return new MaterialApp(home: new Home());
   }
 }
 
